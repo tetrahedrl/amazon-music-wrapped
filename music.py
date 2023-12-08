4bf5d513edda5b719e55f755530a868f46063d3f
Ry <rymcclure73@gmail.com>
2023-12-08 02:28:36 -0500

    v3

--- music.py
+++ music.py
@@ -1,5 +1,4 @@
 import pandas
-import csv
 import numpy as np
 df = pandas.read_csv('Amazon-Music/listening.csv')
 df = df[['2023' in timestamp for timestamp in df.timestamp.values]]
@@ -15,10 +14,17 @@ df = df.groupby(by='asin',as_index=False).agg({
     'consumptionDurationMs':'sum',
     'artistAsin': first_str
 })
-with open('Amazon-Music/library.csv','r',encoding='utf-8') as f:
-    reader = csv.reader(f)
-    artists_dict = {row[16]:row[15] for row in reader}
-    genre_dict = {row[5]:row[19] for row in reader}
+library_df = pandas.read_csv('Amazon-Music/library.csv')
+artists_dict = {row['artistAsin']:row['artistName'] for i,row in library_df.iterrows()}
+genre_dict = {row['asin']:row['primaryGenre'] for i,row in library_df.iterrows()}
 df = df.assign(genre=[genre_dict.get(asin) for asin in df.asin],artist=[artists_dict.get(asin) for asin in df.artistAsin])
 df = df.sort_values(by='consumptionDurationMs')
-print(df[['title','artist','genre','consumptionDurationMs']])
+df[['title','artist','genre','consumptionDurationMs']].to_csv('top_songs.csv')
+sum_by_artist = df.groupby(by='artist',as_index=False).agg({
+    'consumptionDurationMs':'sum'
+})
+sum_by_artist.sort_values(by='consumptionDurationMs').to_csv('top_artists.csv')
+sum_by_genre = df.groupby(by='genre',as_index=False).agg({
+    'consumptionDurationMs':'sum'
+})
+sum_by_genre.sort_values(by='consumptionDurationMs').to_csv('top_genres.csv')
