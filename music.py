8348e235b9b781a19e8a49b08c3e7bffe58870f0
ry.mcclure <ry.mcclure@noaa.gov>
2023-12-10 02:26:54 -0500

    get genres from other songs, albums from artist

--- music.py
+++ music.py
@@ -1,25 +1,55 @@
 import pandas
 import numpy as np
 
-def first_str (x):
-    for element in x:
+def first_str (series):
+    for element in series:
         if isinstance(element,str):
             return element
     return None
 
+def get_genres (df):
+    genres = []
+    for index, row in df[['asin','artistAsin']].iterrows():
+        if row['asin'] in song_genre_dict:
+            genres.append(song_genre_dict[row['asin']])
+            continue
+        elif row['asin'] in album_genre_dict:
+            genres.append(album_genre_dict[row['asin']])
+            continue
+        elif row['artistAsin'] in artist_album_genre_dict and artist_album_genre_dict[row['artistAsin']] is not None:
+            genres.append(artist_album_genre_dict[row['artistAsin']])
+        else:
+            genres.append(artist_song_genre_dict.get(row['artistAsin']))
+    return genres
+
+def pick_primary (series):
+    mode = series.mode()
+    if not len(mode):
+        return None
+    return mode[0]
+        
 df = pandas.read_csv('Amazon-Music/listening.csv')
 df = df[['2023' in timestamp for timestamp in df.timestamp.values]]    
 df = df.groupby(by='asin',as_index=False).agg({
     'title': first_str,
+    'artistAsin': first_str,
     'consumptionDurationMs':'sum',
-    'artistAsin': first_str
+    'terminationReason': lambda x: dict(x.value_counts(sort=False))
 })
 library_df = pandas.read_csv('Amazon-Music/library.csv')
 artists_dict = {row['artistAsin']:row['artistName'] for i,row in library_df.iterrows()}
-genre_dict = {row['asin']:row['primaryGenre'] for i,row in library_df.iterrows()}
-df = df.assign(genre=[genre_dict.get(asin) for asin in df.asin],artist=[artists_dict.get(asin) for asin in df.artistAsin])
+song_genre_dict = {row['asin']:row['primaryGenre'] for i,row in library_df.iterrows() if not ((not isinstance(row['primaryGenre'],str)) and np.isnan(row['primaryGenre']))}
+album_genre_dict = {row['asin']:row['albumPrimaryGenre'] for i,row in library_df.iterrows() if not ((not isinstance(row['albumPrimaryGenre'],str)) and np.isnan(row['albumPrimaryGenre']))}
+artist_song_genre_dict = library_df.groupby(by='artistAsin').agg({'primaryGenre':pick_primary}).to_dict()['primaryGenre']
+artist_album_genre_dict = library_df.groupby(by='artistAsin').agg({'albumPrimaryGenre':pick_primary}).to_dict()['albumPrimaryGenre']
+df = df.assign(
+    genre=get_genres(df),
+    artist=[artists_dict.get(asin) for asin in df.artistAsin],
+    starts=[sum(reason_dict.values()) for reason_dict in df.terminationReason],
+    finishes=[reason_dict.get('trackFinished') for reason_dict in df.terminationReason]
+    )
 df = df.sort_values(by='consumptionDurationMs')
-df[['title','artist','genre','consumptionDurationMs']].to_csv('top_songs.csv')
+df[['title','artist','genre','consumptionDurationMs','starts','finishes']].to_csv('top_songs.csv')
 sum_by_artist = df.groupby(by='artist',as_index=False).agg({
     'consumptionDurationMs':'sum'
 })
